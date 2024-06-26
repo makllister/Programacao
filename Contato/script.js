40c419f113a4b1b4a6a1196b6dcf657b1956e8fa
Makllister <makllister_ml@hotmail.com>
2024-06-26 18:04:57 -0300

    Add files via upload

--- Contato/script.js
+++ Contato/script.js
@@ -26,10 +26,27 @@ function conferirMensagemWhatsApp() {
 
     document.getElementById("confNome").textContent = nome
     document.getElementById("confMsg").textContent = mensagem
-    document.getElementById("confDate").textContent = formatar(date);
+    document.getElementById("confDate").textContent = formatar(date)
 }
+enviar(nome, mensagem, date)
+
 
 
 function enviar() {
     console.log(nomeGlobal, mensagemGlobal, dateGlobal)
 }
+
+function enviar() {
+    var numeroTelefone = "5541991029233"
+
+    var linkWhatsapp = "https://wa.me/" +
+        numeroTelefone +
+        "?text-NOME DO RECEPTOR(A): " +
+        nomeGlobal + " - " +
+        mensagemGlobal +
+        "- " +
+        formatar(dateGlobal)
+
+    window.open(linkWhatsapp, "_blank")
+}
+
